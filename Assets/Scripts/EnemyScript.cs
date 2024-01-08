0b0af0e18da662b0a8d087c051a9083447949dd6
Beshoy-Bebawe <118367517+Beshoy-Bebawe@users.noreply.github.com>
2024-01-08 12:48:54 -0500

    I finished 4.4

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -20,5 +20,11 @@ public class EnemyScript : MonoBehaviour
     {
          Vector3 lookDirection = (player.transform.position - transform.position).normalized;
         enemyRb.AddForce( lookDirection * speed);// adds the force to follow the enemy from a safe distance
+        if (transform.position.y < -10)
+        {
+            Destroy(gameObject);
+
+        }
+
     }
 }
